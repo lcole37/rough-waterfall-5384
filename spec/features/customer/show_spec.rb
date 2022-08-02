2f215d08939560e9c00ecbe3003d30869dc87dab
Lucas Colwell <colwellc37@gmail.com>
2022-08-02 15:33:42 -0600

    Test: Create story1 test

---
+++ spec/features/customer/show_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'customer show page' do
+
+  lowes = Supermarket.create!(name: "Lowes", location: "Denver")
+  sooper = Supermarket.create!(name: "King Sooper", location: "Denver")
+  depot = Supermarket.create!(name: "Home Depot", location: "Denver")
+
+  holly = sooper.customers.create!(name: "Holly")
+  michael = lowes.customers.create!(name: "Michael")
+  chris = sooper.customers.create!(name: "Chris")
+  boomer = depot.customers.create!(name: "Boomer")
+
+  sauce = Item.create!(name: "sauce", price: 5)
+  steak = Item.create!(name: "steak", price: 15)
+  broom = Item.create!(name: "broom", price: 10)
+  soap = Item.create!(name: "soap", price: 10)
+  lightbulb = Item.create!(name: "lightbulb", price: 5)
+  ball = Item.create!(name: "ball", price: 5)
+  ladder = Item.create!(name: "ladder", price: 50)
+
+  CustomerItem.create!(customer_id: holly.id, item_id: sauce.id)
+  CustomerItem.create!(customer_id: holly.id, item_id: steak.id)
+  CustomerItem.create!(customer_id: holly.id, item_id: soap.id)
+
+  CustomerItem.create!(customer_id: michael.id, item_id: broom.id)
+  CustomerItem.create!(customer_id: michael.id, item_id: lightbulb.id)
+  CustomerItem.create!(customer_id: michael.id, item_id: soap.id)
+
+  visit "/customer/#{holly.id}"
+
+  expect(page).to have_content("Holly")
+  expect(page).to have_content("sauce")
+  expect(page).to have_content("soap")
+  expect(page).to have_content("King Sooper")
+  expect(page).not_to have_content("ladder")
+  expect(page).not_to have_content("Lowes")
+
+  visit "/customer/#{michael.id}"
+
+  expect(page).to have_content("Michael")
+  expect(page).to have_content("broom")
+  expect(page).to have_content("soap")
+  expect(page).to have_content("Lowes")
+  expect(page).not_to have_content("steak")
+  expect(page).not_to have_content("Home Depot")
+end
