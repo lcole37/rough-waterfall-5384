79f1691ceb9872e4e36252bb7d5422a9a97fb1da
Lucas Colwell <colwellc37@gmail.com>
2022-08-02 15:34:03 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lowes = Supermarket.create!(name: "Lowes", location: "Denver")
+sooper = Supermarket.create!(name: "King Sooper", location: "Denver")
+depot = Supermarket.create!(name: "Home Depot", location: "Denver")
+
+miguel = Customer.create!(name: "Miguel")
+holly = Customer.create!(name: "Holly")
+chris = Customer.create!(name: "Chris")
+boomer = Customer.create!(name: "Boomer")
+
+sauce = Item.create!(name: "sauce", price: 5)
+steak = Item.create!(name: "steak", price: 15)
+broom = Item.create!(name: "broom", price: 10)
+lightbulb = Item.create!(name: "lightbulb", price: 10)
+ball = Item.create!(name: "ball", price: 5)
