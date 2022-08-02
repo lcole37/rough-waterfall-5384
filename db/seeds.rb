37dae034308cafb3372f481c121a2ca91e9e3b73
Lucas Colwell <colwellc37@gmail.com>
2022-08-02 16:00:25 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Supermarket.destroy_all
+Customer.destroy_all
+Item.destroy_all
 
 lowes = Supermarket.create!(name: "Lowes", location: "Denver")
 sooper = Supermarket.create!(name: "King Sooper", location: "Denver")
